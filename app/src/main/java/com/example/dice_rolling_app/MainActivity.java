c1644a62bfd1bbca349af61b05224d5d58cf3e07
Dominic <dominictojan@gmail.com>
2026-03-02 19:31:13 +0530

    button clicked action done

---
+++ app/src/main/java/com/example/dice_rolling_app/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.dice_rolling_app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+
+        });
+        Button roll =(Button) findViewById(R.id.rollDice);
+        TextView result =(TextView) findViewById(R.id.Result);
+        roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText("button clicked");
+            }
+        });
+
+    }
+}
